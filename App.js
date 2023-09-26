269e8d76ef5c945809d2eddb51310ceb60c35526
loktoidi <loktoidi@luukku.com>
2023-09-26 14:12:58 +0300

    uusi

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TextInput, Button, Keyboard } from 'react-native';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import styles from './style/style'; // Import your styles
 
 export default function App() {
+  const [playerName, setPlayerName] = useState('');
+  const [isInputFocused, setInputFocused] = useState(true);
+
+  useEffect(() => {
+    // Automatically focus the input field when the component mounts
+    if (isInputFocused) {
+      inputRef.focus();
+    }
+  }, [isInputFocused]);
+
+  // Function to handle the OK button press
+  const handleOKPress = () => {
+    // You can handle submitting the player's name here
+    // For now, we'll just close the keyboard and set focus to false
+    Keyboard.dismiss();
+    setInputFocused(false);
+  };
+
+  let inputRef; // Declare inputRef here
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.pagename}>Home</Text>
+      <Header />
+      <TextInput
+        ref={(input) => {
+          inputRef = input; // Reference to the input field
+        }}
+        placeholder={playerName ? '' : 'Enter Player Name'} // Use conditional placeholder
+        placeholderTextColor={styles.placeholderText.color}
+        value={playerName}
+        onChangeText={(text) => setPlayerName(text)}
+        onSubmitEditing={handleOKPress}
+        style={styles.playerNameInput}
+      />
+
+      <Button title="OK" onPress={handleOKPress} />
+      <Footer />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ style/style.js
@@ -0,0 +1,84 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  pagename: {
+    marginTop: 25,
+    fontSize: 24, 
+    textAlign: 'left',
+    color: '#84d710'
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#065306'
+  },
+  header: {
+    marginTop: 30,
+    marginBottom: 15,
+    backgroundColor: 'skyblue',
+    flexDirection: 'row',
+  },
+  footer: {
+    marginTop: 20,
+    backgroundColor: 'skyblue',
+    flexDirection: 'row'
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 23,
+    textAlign: 'center',
+    margin: 10,
+  },
+  placeholderText: {
+color: '#ffffff',
+fontSize: 16,
+  }, 
+  playerNameInput: {
+    color: '#98d438',
+    fontSize: 18,
+  },
+  
+  author: {
+    color: '#fbfdf9',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 10,
+  },
+  gameboard: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  gameinfo: {
+    backgroundColor: '#fff',
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 20,
+    marginTop: 10
+  },
+  row: {
+    marginTop: 20,
+    padding: 10
+  },
+  flex: {
+    flexDirection: "row"
+  },
+  button: {
+    margin: 30,
+    flexDirection: "row",
+    padding: 10,
+    backgroundColor: "#73CED6",
+    width: 150,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color:"#2B2B52",
+    fontSize: 20
+  }
+});
